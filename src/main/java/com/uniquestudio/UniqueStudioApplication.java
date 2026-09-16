88b9d3df82bd8be6970d6d292c2114f38e45e8a0
shubhambhoknal <shubhambbhoknal@gmail.com>
2026-09-16 23:42:12 +0530

    First commit of my Spring Boot project

---
+++ src/main/java/com/uniquestudio/UniqueStudioApplication.java
@@ -0,0 +1,14 @@
+package com.uniquestudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueStudioApplication.class, args);
+		System.out.println("Studio Application");
+	}
+
+}
---
+++ src/test/java/com/uniquestudio/UniqueStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquestudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
